329dc4a925ee6d4854d5cf17660a88fcbcc2e195
King <jack3.king@live.uwe.ac.uk>
2019-10-23 21:56:05 +0100

    Audio and Game Manager

---
+++ Code Crunch/Assets/Managers/GameManager/DontDestroyOnLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+   void Awake()
+    {
+        DontDestroyOnLoad(this);
+        if (GameObject.Find("GameManager"))
+        {
+            Destroy(this);
+        }
+    }
+}
