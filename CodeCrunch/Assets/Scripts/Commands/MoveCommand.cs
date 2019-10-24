18504c59eae0dc54969412550b732f408bc8d7a8
King <jack3.king@live.uwe.ac.uk>
2019-10-24 19:38:44 +0100

    Updated to right version of Unity

---
+++ CodeCrunch/Assets/Scripts/Commands/MoveCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCommand : MonoBehaviour
+{
+
+    //OnCollisionStay and input released.
+    //Add this command to the back of the array for specified player.
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        //
+    }
+}
