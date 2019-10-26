e6d813b6260412367cc19d3a28b62b6dfa86a799
King <jack3.king@live.uwe.ac.uk>
2019-10-26 04:31:52 +0100

    Test command with drag from touch

--- CodeCrunch/Assets/Scripts/MoveByTouch.cs
+++ CodeCrunch/Assets/Scripts/MoveByTouch.cs
@@ -9,6 +9,8 @@ public class MoveByTouch : MonoBehaviour
     float timeStep;
     float speed = 1;
 
+    public bool selected = false;
+
     //public GameObject target;
     void Update()
     {
@@ -16,20 +18,39 @@ public class MoveByTouch : MonoBehaviour
 
         if (Input.touchCount > 0)
         {
+            Touch touch = Input.GetTouch(0);
             Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
             RaycastHit hit;
             Debug.DrawRay(ray.origin, ray.direction * 100, Color.yellow, 10f);
             if (Physics.Raycast(ray, out hit))
             {
                 Debug.Log(hit.transform.name);
-                if (hit.collider != null)
+                if (hit.collider.CompareTag("Command"))
                 { 
                     GameObject touchedObject = hit.transform.gameObject;
+                    selected = true;
+                }
 
-                    Debug.Log("Touched " + touchedObject.transform.name);
+                if (selected)
+                {
+                    Debug.Log("Moving!");
+                    Vector3 target = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 1.0f));
+                    transform.position = Vector3.MoveTowards(transform.position, target
+                        , timeStep);
+
+                   
                 }
             }
+
+            if(touch.phase == TouchPhase.Ended && selected)
+            {
+                selected = false;
+                Debug.Log("User released cube");
+            }
+
+
         }
+
 
     }
 }
