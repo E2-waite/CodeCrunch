239dbd2e40bf33de511613622faadcd79a39ea24
JackLynham <Jack2.Lynham@live.uwe.ac.uk>
2019-10-28 21:12:01 +0000

    Merge

--- CodeCrunch/Assets/Managers/CommandManager/CommandSpawnManager.cs
+++ CodeCrunch/Assets/Managers/CommandManager/CommandSpawnManager.cs
@@ -31,35 +31,6 @@ public class CommandSpawnManager : MonoBehaviour
 
     private void Update()
     {
-
-
-    switch (commandToSpawn)
-        {
-            case 5:
-                CommandText.text = ("Random()");
-                break;
-            case 4:
-                CommandText.text = ("Left()");
-                Rm.Left();
-                break;
-            case 3:
-                CommandText.text = ("Right()");
-                Rm.Right();
-                break;
-            case 2:
-                CommandText.text = ("Back");
-                Rm.Down();
-                break;
-            case 1:
-                CommandText.text = ("Shoot");
-                break;
-            default:
-                CommandText.text = ("Forward()");
-                Rm.Forward();
-                break;
-        }
-
-
         if (t <= 0)
         {
             spawnRange = new Vector3(Random.Range(-100f, 100f), 0.75f, 0);
