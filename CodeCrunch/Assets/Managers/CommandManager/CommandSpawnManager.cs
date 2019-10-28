523c0bdb55fe624ee7c427696cb9ac03df29f0ad
JackLynham <Jack2.Lynham@live.uwe.ac.uk>
2019-10-28 21:05:48 +0000

    Forgot to push Hours ago

--- CodeCrunch/Assets/Managers/CommandManager/CommandSpawnManager.cs
+++ CodeCrunch/Assets/Managers/CommandManager/CommandSpawnManager.cs
@@ -13,48 +13,76 @@ public class CommandSpawnManager : MonoBehaviour
     public GameObject[] commands;
     private Vector3 spawnRange;
     public int commandToSpawn;
+    public RobotMovement Rm;
 
-    private Vector3 spawnRange;
     private GameObject uiCanvas;
+
+    public Text CommandText;
 
     private void Start()
     {
 
-    {
-        uiCanvas = GameObject.FindGameObjectWithTag("Canvas");
+        {
+            uiCanvas = GameObject.FindGameObjectWithTag("Canvas");
+        }
+
     }
 
 
     private void Update()
     {
-        if (t <= 0)
-    private void Update()
-    {
-        if(t <= 0)
+
+
+    switch (commandToSpawn)
         {
-            spawnRange = new Vector3(Random.Range(-1f, 1f), 0.75f, 0);
-            commandToSpawn = Random.Range(0, commands.Length - 1);
-            Instantiate(commands[commandToSpawn], spawnRange, Quaternion.identity);
+            case 5:
+                CommandText.text = ("Random()");
+                break;
+            case 4:
+                CommandText.text = ("Left()");
+                Rm.Left();
+                break;
+            case 3:
+                CommandText.text = ("Right()");
+                Rm.Right();
+                break;
+            case 2:
+                CommandText.text = ("Back");
+                Rm.Down();
+                break;
+            case 1:
+                CommandText.text = ("Shoot");
+                break;
+            default:
+                CommandText.text = ("Forward()");
+                Rm.Forward();
+                break;
+        }
+
+
+        if (t <= 0)
+        {
             spawnRange = new Vector3(Random.Range(-100f, 100f), 0.75f, 0);
-            int commandToSpawn = Random.Range(0, commands.Length - 1);
+            commandToSpawn = Random.Range(0, commands.Length - 1);
             GameObject uiCommand = Instantiate(commands[commandToSpawn], spawnRange, Quaternion.identity, uiCanvas.transform);
             uiCommand.GetComponent<RectTransform>().localPosition = new Vector2(0f, 600f);
             spawnTimer = Random.Range(minSpawnTime, maxSpawnTime);
             t = spawnTimer;
 
-          
         }
+
 
         else
         {
             t -= Time.deltaTime;
         }
+
+
+
+
     }
 
 
 
-
-
 }
-
 
