6c43d3a9016f96134c69bc9ae64161484fa5be7c
King <jack3.king@live.uwe.ac.uk>
2019-10-28 16:13:29 +0000

    Smoother Touch Control with UI Elements

---
+++ CodeCrunch/Assets/moveUiWithTouch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+
+public class moveUiWithTouch : MonoBehaviour
+{
+    GraphicRaycaster m_Raycaster;
+    PointerEventData m_PointerEventData;
+    EventSystem m_EventSystem;
+
+    void Start()
+    {
+        //Fetch the Raycaster from the GameObject (the Canvas)
+        m_Raycaster = GetComponent<GraphicRaycaster>();
+        //Fetch the Event System from the Scene
+        m_EventSystem = GetComponent<EventSystem>();
+    }
+
+    void Update()
+    {
+        //Check if the left Mouse button is clicked
+        if (Input.touchCount > 0)
+        {
+            for(int i = 0; i < Input.touchCount; ++i)
+            {
+                //Set up the new Pointer Event
+                m_PointerEventData = new PointerEventData(m_EventSystem);
+                //Set the Pointer Event Position to that of the touch position
+                m_PointerEventData.position = Input.GetTouch(i).position;
+
+                //Create a list of Raycast Results
+                List<RaycastResult> results = new List<RaycastResult>();
+
+                //Raycast using the Graphics Raycaster and mouse click position
+                m_Raycaster.Raycast(m_PointerEventData, results);
+
+                //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+                foreach (RaycastResult result in results)
+                {
+
+                    if (result.gameObject.CompareTag("Command"))
+                    {
+                        Debug.Log("Hit " + result.gameObject.tag);
+                        result.gameObject.transform.position = Input.GetTouch(i).position;
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ CodeCrunch/Assets/uiCollision.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class uiCollision : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision/Trigger");
+        if (other.gameObject.CompareTag("Command"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
