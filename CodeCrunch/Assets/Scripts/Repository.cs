f9582ef23cfe5420b5ee36223ba62978604a1d6c
King <jack3.king@live.uwe.ac.uk>
2019-10-30 13:29:40 +0000

    Working on 1 after the other for commands

--- CodeCrunch/Assets/Scripts/Repository.cs
+++ CodeCrunch/Assets/Scripts/Repository.cs
@@ -6,45 +6,42 @@ using EventTypes;
 public class Repository : MonoBehaviour
 {
     [SerializeField] private int RepositoryID;
-    [SerializeField] private List<Cmd> commandList;
+    [SerializeField] private List<Cmd.CommandType> commandList;
     [SerializeField] private GameObject gameGrid;
 
-    public void addCommandToRepo(Cmd _command)
+    public void addCommandToRepo(Cmd.CommandType _command)
     {
         commandList.Add(_command);
         gameGrid = GameObject.FindGameObjectWithTag("Grid");
-        Invoke("FindRobot", 1);
     }
-
 
     public void doCommand()
     {
         if(commandList.Count > 0)
         {
             //Switch statement to decide which function to call on robot.
-            switch(commandList[0].cmd)
+
+            for(int i = 0; i < commandList.Count; ++i)
             {
-                case Cmd.CommandType.move:
+                switch (commandList[i])
                 {
-                    gameGrid.GetComponent<Grid>().GetRobot(RepositoryID).GetComponent<RobotMovement>().MoveRobot(0, 1);
-                    break;
-                }
-                case Cmd.CommandType.rotate:
-                {
-                        //Deciding direction to turn.
-                        bool clockwise = false;
-                        if(commandList[0].rot == Cmd.RotationDirection.clockwise)
-                        {
-                            clockwise = true;
-                        }
-                        //End
-                        gameGrid.GetComponent<Grid>().GetRobot(RepositoryID).GetComponent<RobotMovement>().RotateRobot(clockwise);
+                    case Cmd.CommandType.move:
+                    {
+                        gameGrid.GetComponent<Grid>().GetRobot(RepositoryID).GetComponent<RobotMovement>().MoveRobot(0, 1);
                         break;
+                    }
+                    case Cmd.CommandType.rotateclockwise:
+                    {
+                        gameGrid.GetComponent<Grid>().GetRobot(RepositoryID).GetComponent<RobotMovement>().RotateRobot(true);
+                        break;
+                    }
+                    case Cmd.CommandType.rotatecounterclockwise:
+                    {
+                        gameGrid.GetComponent<Grid>().GetRobot(RepositoryID).GetComponent<RobotMovement>().RotateRobot(false);
+                        break;
+                    }
                 }
-            }
-            
+            }    
         }
-        
-        
     }
 }
