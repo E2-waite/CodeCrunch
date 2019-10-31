27926296f8a188368a54c44d1133ff47aa95b5ca
King <jack3.king@live.uwe.ac.uk>
2019-10-31 02:16:05 +0000

    Added click effects on compile button

---
+++ CodeCrunch/Assets/Scripts/Commands/UI_FX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_FX : MonoBehaviour
+{
+    public GameObject clickFX;
+    public GameObject glowFX;
+    public GameObject origin;
+
+
+    public void SpawnClickedVFX()
+    {
+        if(clickFX != null)
+        {
+            var vfx = Instantiate(clickFX, origin.transform.position, Quaternion.identity) as GameObject;
+            vfx.transform.SetParent(origin.transform);
+
+            var ps = vfx.GetComponent<ParticleSystem>();
+            Destroy(vfx, ps.main.duration + ps.main.startLifetime.constantMax);
+        }
+        if(glowFX != null)
+        {
+
+        }
+    }
+}
