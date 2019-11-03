c3cfab6ba68dfc960ce05b3c7c550da985aaf96a
King <jack3.king@live.uwe.ac.uk>
2019-11-03 19:58:02 +0000

    Fade in and Fade Out For Scenes

---
+++ CodeCrunch/Assets/FadeIn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeIn : MonoBehaviour
+{
+    float transitionTimer = 2.0f;
+    public GameObject FadePanel;
+
+    void Update()
+    {
+        transitionTimer -= Time.deltaTime;
+        var tempColour = FadePanel.GetComponent<Image>().color;
+        float fadeAlpha = Mathf.Lerp(0, 1, transitionTimer);
+        tempColour.a = fadeAlpha;
+        FadePanel.GetComponent<Image>().color = tempColour;
+        if (transitionTimer <= 0)
+        {
+            Destroy(FadePanel);
+        }
+    }
+}
--- CodeCrunch/Assets/MainMenu.cs
+++ CodeCrunch/Assets/MainMenu.cs
@@ -2,14 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-    
+    float transitionTimer = 2.0f;
+    bool sceneChangePlay = false;
+    public GameObject FadePanel;
+
+    private void Update()
+    {
+        if(sceneChangePlay)
+        {
+            transitionTimer -= Time.deltaTime;
+            var tempColour = FadePanel.GetComponent<Image>().color;
+            float fadeAlpha = Mathf.Lerp(1, 0, transitionTimer);
+            tempColour.a = fadeAlpha;
+            FadePanel.GetComponent<Image>().color = tempColour;
+            if (transitionTimer <= 0)
+            {
+                sceneChangePlay = false;
+                SceneManager.LoadScene(1);
+            }
+        }
+    }
+
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
-        Debug.Log("PLAY");
+        FadePanel.SetActive(true);
+        sceneChangePlay = true;
     }
 
     public void QuitGame()
