b2177216a355d04335ebf431f3497234f97a0e71
King <jack3.king@live.uwe.ac.uk>
2019-11-04 02:06:07 +0000

    Working on highscore

---
+++ CodeCrunch/Assets/ScoreManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ScoreManager : MonoBehaviour
+{
+    public bool SetHighScore(int score)
+    {
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+            return true;
+        }
+
+        return false;
+    }
+
+    public int GetCurrentHighScore()
+    {
+        return PlayerPrefs.GetInt("HighScore", 0); ;
+    }
+
+
+}
