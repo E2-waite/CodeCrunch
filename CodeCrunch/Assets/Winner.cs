e1ae9d152b883c7b7e631f2ada37a289ca74c11f
King <jack3.king@live.uwe.ac.uk>
2019-11-05 02:21:13 +0000

    Winner Working

--- CodeCrunch/Assets/Winner.cs
+++ CodeCrunch/Assets/Winner.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class Winner : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.CompareTag("Robot"))
-        {
+    public GameObject WinnerMenu;
+    public GameObject[] Repositorys;
+    public GameObject Spawnmanager;
+    bool displayWinnerMenu = false;
+    float timerDelay = 2.0f;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Robot"))
+        {
+            displayWinnerMenu = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (displayWinnerMenu)
+        {
+            timerDelay -= Time.deltaTime;
+            if (timerDelay <= 0)
+            {
+                WinnerMenu.SetActive(true);
+                foreach(var repo in Repositorys)
+                {
+                    repo.SetActive(false);
+                }
+                var allCommands = GameObject.FindGameObjectsWithTag("Command");
+                foreach(GameObject cmd in allCommands)
+                {
+                    Destroy(cmd);
+                }
+                Spawnmanager.SetActive(false);
+                Destroy(gameObject);
+            }
         }
     }
 }
