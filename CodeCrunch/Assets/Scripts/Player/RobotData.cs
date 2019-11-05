1d69b299648b4773ccab76115caa4db88920da22
e2-waite <Edward2.waite@gmail.com>
2019-11-05 15:49:31 +0000

    Update robot model's colour based on player num

--- CodeCrunch/Assets/Scripts/Player/RobotData.cs
+++ CodeCrunch/Assets/Scripts/Player/RobotData.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RobotData : MonoBehaviour
 {
-    int player_num;
+    public int player_num = 5;
     Renderer player_rend;
     bool colour_set;
     // Start is called before the first frame update
@@ -14,36 +14,77 @@ public class RobotData : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (!colour_set)
-        {
-            if (player_num == 0)
-            {
-                player_rend.material.SetColor("_Color", Color.red);
-                colour_set = true;
-            }
-            else if (player_num == 1)
-            {
-                player_rend.material.SetColor("_Color", Color.blue);
-                colour_set = true;
-            }
-            else if (player_num == 2)
-            {
-                player_rend.material.SetColor("_Color", Color.green);
-                colour_set = true;
-            }
-            else if (player_num == 3)
-            {
-                player_rend.material.SetColor("_Color", Color.yellow);
-                colour_set = true;
-            }
-        }
-    }
+    //void Update()
+    //{
+    //    if (!colour_set)
+    //    {
+    //        if (player_num == 0)
+    //        {
+    //            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //            {
+    //               r.material.color = Color.red;
+    //            }
+    //            colour_set = true;
+    //        }
+    //        else if (player_num == 1)
+    //        {
+    //            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //            {
+    //                r.material.color = Color.blue;
+    //            }
+    //            colour_set = true;
+    //        }
+    //        else if (player_num == 2)
+    //        {
+    //            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //            {
+    //                r.material.color = Color.green;
+    //            }
+    //            colour_set = true;
+    //        }
+    //        else if (player_num == 3)
+    //        {
+    //            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+    //            {
+    //                r.material.color = Color.yellow;
+    //            }
+    //            colour_set = true;
+    //        }
+    //    }
+    //}
 
     public void SetPlayerNum(int num)
     {
         player_num = num;
+
+        if (player_num == 0)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.material.color = Color.red;
+            }
+        }
+        if (player_num == 1)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.material.color = Color.blue;
+            }
+        }
+        if (player_num == 2)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.material.color = Color.green;
+            }
+        }
+        if (player_num == 3)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            {
+                r.material.color = Color.yellow;
+            }
+        }
     }
 
     public int GetPlayerNum()
